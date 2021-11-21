50fd051c42367dcc8dfa00d6a3ab7adcf13bebed
salvadocs <salvag143@gmail.com>
2021-11-21 18:31:39 +0100

    Clase Empleado
    Clase Empleado con atributos,constructores y métodos get -set.

--- src/Modelo/Empleado.java
+++ src/Modelo/Empleado.java
@@ -4,14 +4,91 @@
  */
 package Modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author Salva
  */
 public class Empleado {
     
+    private int numero;
+    private String nombre;
+    private String apellido;
+    private String foto;
+    private float sueldo;
+    private float sueldoMax;
+    private Date fechaAlta;
+        
+    
     public Empleado(){
     
     }
     
+    public Empleado(int numero, String nombre, String apellido, String foto, float sueldo, float sueldoMax, Date fechaAlta){
+        this.numero = numero;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.foto = foto;
+        this.sueldo = sueldo;
+        this.sueldoMax = sueldoMax;
+        this.fechaAlta = fechaAlta;
+    }
+    //Metodos get
+    public int getNumero(){
+        return numero;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getapellido(){
+        return apellido;
+    }
+    
+    public String getFoto(){
+        return foto;
+    }
+    
+    public float getSueldo(){
+        return sueldo;
+    }
+    
+    public float getSueldoMax(){
+        return sueldoMax;
+    }
+    
+    public Date getFechaAlta(){
+        return fechaAlta;
+    }
+    
+    //Metodos set
+    public void setNumero(){
+        this.numero = numero;
+    }
+    
+    public void setNombre(){
+        this.nombre = nombre;
+    }
+
+    public void setApellido(){
+        this.apellido = apellido;
+    }     
+    
+    public void setFoto(){
+        this.foto = foto;
+    }     
+    
+    public void setSueldo(){
+        this.sueldo = sueldo;
+    }  
+    
+    public void setSueldoMax(){
+        this.sueldoMax = sueldoMax;
+    }  
+    
+    public void setFechaAlt(){
+        this.fechaAlta = fechaAlta;
+    }  
 }
